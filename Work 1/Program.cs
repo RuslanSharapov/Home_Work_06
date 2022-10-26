aebc25a266cdb0b96aca3bc1868431cd6885b005
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-26 16:01:35 +0300

    Показать двумерный массив размером m,n заполненный вещественными числами

---
+++ Work 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();    
+    }
+}
+
+
+Console.Write("введите количество столбцов m: ");
+int m = int.Parse(Console.ReadLine()?? "0");
+Console.Write("введите количество строк n: ");
+int n = int.Parse(Console.ReadLine()?? "0");
+
+double[,] array = new double[m, n];
+
+FillArray(array);
+PrintArray(array);
